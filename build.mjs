b220bfb24a5fa581ce6c5f5fb8664cb924cc0f69
felix-123-crypto <felixzhang0528@gmail.com>
2026-09-21 16:25:19 -0700

    Add sitemap and robots directives

--- build.mjs
+++ build.mjs
@@ -8,7 +8,7 @@ const client = join(dist, 'client');
 const files = [
   'index.html','styles.css','theme.css','app.js','economy.js','shop.html','shop.js','game.html','games.js',
   'voxel.html','voxel.js','football.html','football.js',
-  'classics.html','classics.js','manifest.webmanifest','sw.js','og.png','audiowide.ttf','logo-arcade.png'
+  'classics.html','classics.js','manifest.webmanifest','sw.js','og.png','audiowide.ttf','logo-arcade.png','sitemap.xml','robots.txt'
 ];
 
 await rm(dist, { recursive: true, force: true });
